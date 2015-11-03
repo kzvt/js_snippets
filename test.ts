82a8700d46ff80fb372efad323fb88fcddb1e44d
unknown <jkonopinski@PLLL0237.rule.lan>
2015-11-03 14:02:13 +0100

    typescript

---
+++ test.ts
@@ -0,0 +1,12 @@
+/// <reference path="angular.d.ts"/>
+module $ { 1:app.module } {
+    'use strict';
+    export default class AppController {
+        static $inject = ['$scope'];
+        constructor($scope: ng.IScope) {
+
+        }
+        action() {
+        }
+    }
+}
